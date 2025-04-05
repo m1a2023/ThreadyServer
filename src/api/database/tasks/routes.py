8ffa1573e3792235620076ba89e112511d3b78c7
Aleksey Salamatin <asalamatin640@gmail.com>
2025-04-05 23:59:37 +0500

    upgrade routs for tasks

--- src/api/database/tasks/routes.py
+++ src/api/database/tasks/routes.py
@@ -10,7 +10,7 @@ from typing import Any, Dict, List, Optional, Union
 """ Internal imports """
 from core.db import SessionDep, get_db
 from services import general_service as gen
-from models.db_models import TaskBase, TaskUpdate, Tasks
+from models.db_models import TaskBase, TaskUpdate, Tasks, ReminderBase, ReminderUpdate, Reminders
 
 
 """ APIRouter added to upper router(db/__init__.py) """
@@ -28,7 +28,7 @@ async def get_all_tasks(s: SessionDep):
 
 @router.get("/project/{id}", dependencies=[Depends(get_db)], response_model=List[Tasks])
 async def get_all_tasks_by_project_id(s: SessionDep, id: int):
-	try: 
+	try:
 		tasks = await gen.get_all_task_by_project_id(s, id)
 		if tasks is None:
 			raise HTTPException(status_code=404, detail=f"Error getting tasks by project_id {id}")
@@ -38,7 +38,7 @@ async def get_all_tasks_by_project_id(s: SessionDep, id: int):
 
 @router.get("/{id}", dependencies=[Depends(get_db)], response_model=Tasks)
 async def get_task_by_id(s: SessionDep, id: int):
-	try: 
+	try:
 		task = await gen.get_task_by_id(s, id)
 		if task is None:
 			raise HTTPException(status_code=404, detail=f"Error getting tasks by project_id {id}")
@@ -47,8 +47,9 @@ async def get_task_by_id(s: SessionDep, id: int):
 		raise HTTPException(status_code=500, detail=f"Erorr getting tasks by project_id {id}: {str(e)}")
 
 @router.post("/", dependencies=[Depends(get_db)], response_model=int)
-async def create_task(s: SessionDep, task: TaskBase):
+async def create_task(s: SessionDep, task: TaskBase, reminder: ReminderBase):
 	try:
+		await gen.create_remider(s, reminder)
 		return await gen.create_task(s, task)
 	except SQLAlchemyError as e:
 		raise HTTPException(status_code=500, detail=f"Error creating task: {str(e)}")
@@ -59,21 +60,22 @@ async def create_tasks(s: SessionDep, tasks: List[TaskBase]):
 		ids = await gen.create_tasks(s, tasks)
 		if ids is None:
 			raise HTTPException(status_code=404, detail="Error creation tasks")
-		return ids 
+		return ids
 	except SQLAlchemyError as e:
 		raise HTTPException(status_code=500, detail=f"Error creating tasks: {str(e)}")
 
 @router.put("/{id}", dependencies=[Depends(get_db)], response_model=int)
-async def update_task_by_id(s: SessionDep, id: int, upd: TaskUpdate):
+async def update_task_by_id(s: SessionDep, id: int, updTask: TaskUpdate, updReminder: ReminderUpdate):
 	try:
-		return await gen.update_task_by_id(s, id, upd)
+		await gen.update_reminder_by_task_id(s, id, updReminder)
+		return await gen.update_task_by_id(s, id, updTask)
 	except SQLAlchemyError as e:
 		raise HTTPException(status_code=500, detail=f"Error updating task by id {id}: {str(e)}")
 
 @router.delete("/{id}", dependencies=[Depends(get_db)], response_model=int)
 async def delete_task_by_id(s: SessionDep, id: int):
 	try:
+		await gen.delete_reminder_by_task_id(s, id)
 		return await gen.delete_task_by_id(s, id)
 	except SQLAlchemyError as e:
 		raise HTTPException(status_code=500, detail=f"Error deleting task by id {id}: {str(e)}")
-
